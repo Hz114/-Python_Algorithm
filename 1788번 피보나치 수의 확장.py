77a2a9d4ee5c710e31e624b1310f59e64453fb02
Hz114 <kauphj17@gmail.com>
2021-06-06 20:30:47 +0900

    first commit

---
+++ 1788번 피보나치 수의 확장.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+dp = [0,1]
+
+for i in range(2,abs(N)+1):
+    dp.append(dp[i-1]+dp[i-2]%1000000000)
+
+if N == 0:
+    print("0")
+elif N < 0 and N % 2 == 0:
+    print("-1")
+else:
+    print("1")
+
+
+print(dp[abs(N)])
+
+    
