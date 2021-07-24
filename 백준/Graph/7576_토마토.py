85cc7d2c3ed570b951327e7471fc5ed403f64e5a
Hz114 <kauphj17@gmail.com>
2021-07-24 15:55:13 +0900

    update 2021-07-24

--- 백준/Graph/7576_토마토.py
+++ 백준/Graph/7576_토마토.py
@@ -1,47 +1,43 @@
-def BFS(maps, rootNode, maxNode, visit):
+from collections import deque
+
+def BFS(box, ripe, maxNode):
     dx = [0, 0, -1, 1]
     dy = [-1, 1, 0, 0]
-    visit[rootNode[0]][rootNode[1]] = 1
-    queue = [rootNode]
 
-    days = 0
+    days = -1
 
-    while queue:
-        node = queue.pop(0)
-        flag = False
-        for i in range(4):
-            nx = node[0] + dx[i]
-            ny = node[1] + dy[i]
+    while ripe:
+        days += 1
+        for _ in range(len(ripe)):
+            node = ripe.popleft()
 
-            if (0<=nx<=maxNode[0] and 0<=ny<=maxNode[1]) and (maps[nx][ny]==0 and visit[nx][ny]==0):
-                queue.append([nx,ny])
-                visit[nx][ny] = 1
-                flag = True
+            for i in range(4):
+                nx = node[0] + dx[i]
+                ny = node[1] + dy[i]
 
-        if flag:
-            days += 1
+                if (0<=nx<maxNode[0] and 0<=ny<maxNode[1]) and (box[nx][ny]==0):
+                    ripe.append([nx,ny])
+                    box[nx][ny] = 1
 
-    return visit, days
+    # 안익은 토마토가 있으면 -1
+    for b in box:
+        if 0 in b:
+            return -1
+
+    return days
 
 M, N = map(int, input().split())
 
-visited = [[0 for row in range(M)] for col in range(N)]
-maps = []
-for _ in range(N):
-    n = list(map(int, input().split()))
-    maps.append(n)
+box = []
+ripe = deque()
 
-result = 0
-for col in range(N):
-    for row in range(M):
-        if maps[col][row] == 1:
-            visited, days = BFS(maps, [col, row], [N-1, M-1], visited)
-            result += days
+for n in range(N):
+    row = list(map(int, input().split()))
+    for m in range(M):
+        if row[m] == 1:
+            ripe.append([n,m])
+    box.append(row)
 
-print(result)
+days = BFS(box, ripe, [N,M])
 
-
-
-
-print(maps)
-print(visited)
+print(days)
